dd00aaae50c5367031f4f91a48320b34dc5e1e7d
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-12-03 13:23:11 +0100

    Don't start headless shell in background

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM golang:1.12-stretch AS builder
 WORKDIR /go/src/github.com/meinto/website-control-graph
 COPY . .
 
-RUN GO111MODULE=off go get github.com/99designs/gqlgen
+RUN GO111MODULE=on go get github.com/99designs/gqlgen
 RUN GO111MODULE=on go generate ./...
 RUN GO111MODULE=on go get ./...
 RUN GO111MODULE=on GOOS=linux GOARCH=386 go build -o gql-server -ldflags "-X github.com/meinto/website-control-graph/chrome.DockerBuild=yes" server/server.go
--- docker.startup.sh
+++ docker.startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-nohup /headless-shell/headless-shell --no-sandbox --headless --disable-gpu --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 &>/dev/null &
+# nohup /headless-shell/headless-shell --no-sandbox --headless --disable-gpu --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 &>/dev/null &
+
+echo '##########################'
+echo '# starting control graph #'
+echo '##########################'
+echo ''
+
 /gql-server
